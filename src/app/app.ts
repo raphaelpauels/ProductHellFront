ac980561e961989104555097846b2c7915f93f07
Bya Sebastien <byaebastien@hotmail.com>
2026-01-12 08:44:50 +0100

    initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.scss'
+})
+export class App {
+  protected readonly title = signal('TI_Net_2026_Product_Hell.Fend');
+}
